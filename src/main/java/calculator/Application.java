82596195435b6aebc1443a3699de8ccffd56ebc0
CuriHuS <2542656@naver.com>
2025-10-14 22:13:47 +0900

    feat (input) 사용자 입력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 입력
+        String s = input();
+
+    }
+
+    /**
+     * 덧셈 문자열 입력 함수
+     */
+    public static String input() {
+        System.out.print("덧셈할 문자열을 입력하세요: ");
+        return Console.readLine();
     }
 }
